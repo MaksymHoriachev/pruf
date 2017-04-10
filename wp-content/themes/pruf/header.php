95a68853d35aa11c077bc20133a11b792d301bb3
Maxim Goryachev <maxto4kacom@gmail.com>
2017-04-11 01:05:27 +0300

    fixed nav

--- wp-content/themes/pruf/header.php
+++ wp-content/themes/pruf/header.php
@@ -26,7 +26,7 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-12 site-header-inner">
+				<div class="col-12 site-header-inner">
 
 					<div class="site-branding">
 						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
