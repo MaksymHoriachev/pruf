4f49b29d91aabf957abc5345019082a88341dfea
osviatka <svyatka@ukr.net>
2017-04-11 21:25:54 +0300

    finished front-page

--- wp-content/themes/pruf/front-page.php
+++ wp-content/themes/pruf/front-page.php
@@ -14,34 +14,19 @@
 
 get_header(); ?>
 
-    <div class="col-12 front-container">
-        <h2 class="heading">
-            Pruf
-        </h2>
-        <p class="text-main-page">
-            <span>PR не ради PR</span>
-            Свежее образование в мaтерии паблик рилейшнз
-        </p>
+    <div class="col-12 front-section">
+        <div class="front-container">
+            <h2 class="heading">
+                Pruf
+            </h2>
+            <p class="text-main-page">
+                <span>PR не ради PR</span>
+                Свежее образование в мaтерии паблик рилейшнз
+            </p>
+        </div>
 
-        <div id="primary" class="content-area">
-            <main id="main" class="site-main" role="main">
 
-                <?php
-                while ( have_posts() ) : the_post();
-
-                    get_template_part( 'template-parts/content', 'page' );
-
-                    // If comments are open or we have at least one comment, load up the comment template.
-                    if ( comments_open() || get_comments_number() ) :
-                        comments_template();
-                    endif;
-
-                endwhile; // End of the loop.
-                ?>
-
-            </main><!-- #main -->
-        </div><!-- #primary -->
-
+        
     </div>
 
 
