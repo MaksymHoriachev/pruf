4b2b684b376eaa2f778c4f41c6fae530da6179d5
osviatka <svyatka@ukr.net>
2017-04-11 21:13:31 +0300

    front-page started

--- wp-content/themes/pruf/front-page.php
+++ wp-content/themes/pruf/front-page.php
@@ -14,24 +14,37 @@
 
 get_header(); ?>
 
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
+    <div class="col-12 front-container">
+        <h2 class="heading">
+            Pruf
+        </h2>
+        <p class="text-main-page">
+            <span>PR не ради PR</span>
+            Свежее образование в мaтерии паблик рилейшнз
+        </p>
 
-            <?php
-            while ( have_posts() ) : the_post();
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main" role="main">
 
-                get_template_part( 'template-parts/content', 'page' );
+                <?php
+                while ( have_posts() ) : the_post();
 
-                // If comments are open or we have at least one comment, load up the comment template.
-                if ( comments_open() || get_comments_number() ) :
-                    comments_template();
-                endif;
+                    get_template_part( 'template-parts/content', 'page' );
 
-            endwhile; // End of the loop.
-            ?>
+                    // If comments are open or we have at least one comment, load up the comment template.
+                    if ( comments_open() || get_comments_number() ) :
+                        comments_template();
+                    endif;
 
-        </main><!-- #main -->
-    </div><!-- #primary -->
+                endwhile; // End of the loop.
+                ?>
+
+            </main><!-- #main -->
+        </div><!-- #primary -->
+
+    </div>
+
+
 
 <?php
 
