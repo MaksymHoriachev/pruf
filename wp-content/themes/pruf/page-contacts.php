21e07cc008404815a0186ef935711b6b6b25f3d0
osviatka <svyatka@ukr.net>
2017-04-18 22:24:39 +0300

    plugin contact form

--- wp-content/themes/pruf/page-contacts.php
+++ wp-content/themes/pruf/page-contacts.php
@@ -18,24 +18,24 @@ get_header(); ?>
         <h3 class="heading-border">Наши контакты</h3>
         <div class="text text-contacts">
             <p>
-                Ідеї для матеріалів та статті на розгляд: <a href="#">pruf_ua@gmail.com</a>
+                Идеи для материалов и статьи на рассмотрение: <a href="#">pruf_ua@gmail.com</a>
             </p>
             <p>
-                З питань партнерств чи розміщення інформації: <a href="#">pruf_ua@gmail.com</a>
+                По вопросам партнерства и размещения информации: <a href="#">pruf_ua@gmail.com</a>
             </p>
             <p>
-                Інсайди з життя PRUF: <a href="#">pruf_ua@gmail.com</a>
+                Инсайды из жизни PRUF: <a href="#">pruf_ua@gmail.com</a>
             </p>
             <p>
                 Запитання, пропозиції та промені добра: <a href="#">pruf_ua@gmail.com</a>
             </p>
         </div>
-        <h3 class="header-center">Зворотній зв'язок</h3>
+        <h3 class="header-center">Обратная связь</h3>
         <div class="form-collback row">
             <form action="#" method="post" class="col-sm-6">
-                <label for="discription" class="require text mt-0">Побажання, зауваження чи пропозиції</label>
+                <label for="discription" class="require text mt-0">Пожелания, рекомендации и пропозиции</label>
                 <textarea name="discription" id="discription" class="discription"></textarea>
-                <label for="email" class="text">Email для зворотнього зв'язку (за бажанням)</label>
+                <label for="email" class="text">Email для обратной связи</label>
                 <input type="email" name="email" id="email">
                 <button type="submit" class="btn-more hover-mint btn-form-collback">отправить</button>
             </form>
