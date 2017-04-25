622db2d163fadc0fad05dbf5c11a982f649ca550
osviatka <svyatka@ukr.net>
2017-04-26 01:45:07 +0300

    finished contact form

--- wp-content/themes/pruf/page-contacts.php
+++ wp-content/themes/pruf/page-contacts.php
@@ -15,45 +15,19 @@
 get_header(); ?>
 
     <div class="container">
-        <h3 class="loop-title">Наши контакты</h3>
+        <h3 class="loop-title"><?php the_title(); ?></h3>
         <div class="text-contacts">
-            <p>
-                Идеи для материалов и статьи на рассмотрение: <a href="#">pruf_ua@gmail.com</a>
-            </p>
-            <p>
-                По вопросам партнерства и размещения информации: <a href="#">pruf_ua@gmail.com</a>
-            </p>
-            <p>
-                Инсайды из жизни PRUF: <a href="#">pruf_ua@gmail.com</a>
-            </p>
-            <p>
-                Пожелания, рекомендации и пропозиции: <a href="#">pruf_ua@gmail.com</a>
-            </p>
+            <?php
+            while ( have_posts() ) : the_post();
+
+                the_content( );
+
+                // If comments are open or we have at least one comment, load up the comment template.
+
+
+            endwhile; // End of the loop.
+            ?>
         </div>
-        <h3 class="header-center form-header">Обратная связь</h3>
-        <div class="form-collback row">
-            <form action="#" method="post" class="col-lg-6">
-                <label for="discription" class="required">Пожелания, рекомендации и пропозиции</label>
-                <textarea name="discription" id="discription" class="discription" required></textarea>
-                <label for="email" class="required">Email для обратной связи</label>
-                <input type="email" name="email" id="email" required>
-                <label for="name">Ваше имя</label>
-                <input type="text" name="name" id="name">
-                <button type="submit" class="btn-form">Отправить</button>
-            </form>
-        </div>
-        <? global $wpcf7_contact_form;
-        if ( ! ( $wpcf7_contact_form = wpcf7_contact_form( 233 ) ) )
-            return 'Contact form not found!';
-        $form = $wpcf7_contact_form->form_html();
-        echo $form; ?>
-
-
-
     </div>
-
-
-
-
 <?php
 get_footer();
